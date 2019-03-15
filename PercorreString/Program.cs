cc4e9d27f1cedd76c5e513139e72876689325bdf
PmaiWoW <PmaiWoW@Gmail.com>
2019-03-15 12:37:50 +0000

    Adicionar programa que pede uma string (frase) e um carácter ao utilizador, removendo o carácter pedido da string pedida

---
+++ PercorreString/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace PercorreString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declarar variáveis
+            string s, cString;
+            char c;
+
+            //pedir e guardar input do utilizador
+            Console.WriteLine("Input a full phrase, please:");
+            s = Console.ReadLine();
+
+            //pedir ao utilizador o carácter a remover
+            Console.WriteLine("Input a a char to remove:");
+            cString = Console.ReadLine();
+
+            //converter string em char
+            c = cString[0];
+
+            //ciclo for p/ percorrer a string e verificar os carácteres da 
+            //string e adicioná-los ao array
+            foreach(char cInString in s)
+            {
+                if (cInString != c)
+                {
+                    Console.Write(cInString);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
