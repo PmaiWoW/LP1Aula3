a81ac4786e210f340f76a95e84775308caae98ec
PmaiWoW <PmaiWoW@Gmail.com>
2019-03-15 09:24:55 +0000

    Correções a ++ como sufixo (IncDec) Adicionar teste de -- como prefixo ao programa IncDec

--- IncDec/Program.cs
+++ IncDec/Program.cs
@@ -8,10 +8,17 @@ namespace IncDec
         {
             //++ como sufixo:
             int a, b, c;
-            a = 3;
-            b = -6;
-            c = b++ - a++;
-            Console.WriteLine("C: {0}", c);
+            a = -3;
+            b = 6;
+            c = a++ + b++;
+            Console.WriteLine("Sufixo ++ :\na = -3 b = 6\nc = {0}", c);
+
+            //-- como prefixo
+            a = 7;
+            b = -2;
+            c = --a + --b;
+            Console.WriteLine("\nPrefixo -- :\na = 7 b = -2\nc = {0}", c);
+
             Console.ReadKey();
         }
     }
