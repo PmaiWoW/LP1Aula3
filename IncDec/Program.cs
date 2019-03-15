7b5ddb0ee5ecea585a836aac886e0c947ba6290f
PmaiWoW <PmaiWoW@Gmail.com>
2019-03-15 09:15:27 +0000

    Criado programa para testar ++ como sufixo

---
+++ IncDec/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace IncDec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //++ como sufixo:
+            int a, b, c;
+            a = 3;
+            b = -6;
+            c = b++ - a++;
+            Console.WriteLine("C: {0}", c);
+            Console.ReadKey();
+        }
+    }
+}
